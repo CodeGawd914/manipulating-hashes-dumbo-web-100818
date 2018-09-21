4f367410e98c6a48de1628c7c397cb2830d245df
Travis Desir <travisgdesir31@gmail.com>
2018-09-21 02:53:48 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |name, value|
       if name == favorite_icecream_flavors
-        value.delete
+        value.delete_if 
 
 
   #remember to return your newly altered contacts hash!
